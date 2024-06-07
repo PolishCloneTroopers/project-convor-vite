6caf8d7552b5a11bdd06c5848588183425a6ff55
RybaZPolski <julorybarczyk@gmail.com>
2024-06-07 16:40:38 +0200

    okay actually a only a few improvements for now

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import path from "node:path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from "dotenv"
  
+dotenv.config();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +13,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define:{
+    'process.env': process.env
+  },
   server: {
-    host: "192.168.50.153"
+    host: process.env.FRONTEND_URL,
+    port: parseInt(process.env.FRONTEND_URL_PORT || "5173")
   }
 })
