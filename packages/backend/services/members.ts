7bb12c9ce5d6d82784c50a64d01bbc7b94a60963
RybaZPolski <julorybarczyk@gmail.com>
2024-06-09 21:53:17 +0200

    working backend with listing active members

---
+++ packages/backend/services/members.ts
@@ -0,0 +1,32 @@
+import { sql } from '@vercel/postgres';
+
+export default class MembersService{
+
+
+    public static async listActiveMembers(){
+        try{
+            const result =
+            await sql`
+                SELECT members.id, members.nickname, members.instagram, members.discord_id, members.voivodeship, members.about, members.rank, members.joined,
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,COALESCE(clones.number,unlisted_clones.number))) AS cosplay_numbers, 
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,COALESCE(clones.name,unlisted_clones.name))) AS cosplay_names, 
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,COALESCE(clones.variant,unlisted_clones.variant))) AS cosplay_variants,
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,units.name)) AS cosplay_units,
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,units.color)) AS cosplay_colors,
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,clones.image_path)) AS cosplay_avatars,
+                    json_agg(JSON_BUILD_OBJECT(cosplays.id,cosplays.image_url)) AS cosplay_images
+                    FROM members 
+                    LEFT JOIN cosplays ON cosplays.member_id=members.id 
+                    LEFT JOIN clones ON cosplays.clone_id=clones.id 
+                    LEFT JOIN unlisted_clones ON cosplays.unlisted_id=unlisted_clones.id 
+                    LEFT JOIN units ON units.id=COALESCE(clones.unit_id,unlisted_clones.unit_id) 
+                    WHERE members.status='active' AND members.rank IN ('Kadet','Żołnierz','Kapitan','Sierżant','Komandor')
+                    GROUP BY members.id
+                    ORDER BY members.id
+            `;  
+            return result.rows
+        }catch(e){
+            throw(e)
+        }
+    }
+}
