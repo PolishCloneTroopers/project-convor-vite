a3f6c81418bb0783cc71d30fd8d2a9b3aa4390f2
RybaZPolski <julorybarczyk@gmail.com>
2024-06-14 11:20:30 +0200

    should work now

--- packages/backend/api/members/get-all.ts
+++ packages/backend/api/members/get-all.ts
@@ -3,7 +3,7 @@ import MembersService from '../../services/members';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     try {
-        const members = await MembersService.listActiveMembers();
+        const members = await MembersService.listPublicProfiles();
         return res.status(200).json({members: members});
     } catch (e) {
         let message = 'Unknown Error'
